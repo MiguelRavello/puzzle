e410ae01407e8e9c57bf3b7c44897c90bdf576e8
MiguelRavello <errejota7@gmail.com>
2019-07-02 18:09:54 -0500

    juego iterado 3.0

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@ vector<string> m={
   "X XXX XXXXX XXXXX XXXXX XXX X",
   "X XXX XXXXX XXXXX XXXXX XXX X",
   "X                           X",
-  "X XXX XX XXXXXXXXXXX XX XXX X",
-  "X     XX     XXX     XX     X",
-  "X XXX XXXXXX XXX XXXXXX XXX X",
-  "X XXX XX             XX XXX X",
-  "      XX XXXXXXXXXXX XX      ",
-  "X XXX XX XXXXXXXXXXX XX XXX X",
-  "X XXX XX             XX XXX X",
-  "X XXX XXXXXX XXX XXXXXX XXX X",
-  "X     XX     XXX     XX     X",
-  "X XXX XX XXXXXXXXXXX XX XXX X",
+  "X XXX XXXXXXXXXXXXXXXXX XXX X",
+  "X     XXo  oXXXXXo  oXX     X",
+  "X XXX XX XX XXXXX    XX XXX X",
+  "X XXX X               X XXX X",
+  "X     X     X   X   B X      ",
+  "X XXX X  X  B   B   X X XXX X",
+  "X XXX XX            X X XXX X",
+  "X XXX X   X           X XXX X",
+  "X     X            B  X     X",
+  "X XXX X               X XXX X",
   "X XXX                   XXX X",
   "X XXX XXXX XXXXXXXX XXX XXX X",
   "X XXX XXXX          XXX XXX X",
@@ -25,27 +25,51 @@ vector<string> m={
 };
 
 vector<string> n={
-  "XXXXXXXXXXXXXX XXXXXXXXXXXXXX",
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   "X           XX XX           X",
   "X XXX XXXXX XX XX XXXXX XXX X",
   "X XXX XXXXX XX XX XXXXX XXX X",
   "X                           X",
-  "X XXX XX XXXXXXXXXXX XX XXX X",
-  "X     XX     XXX     XX     X",
-  "X XXX XXXXXX XXX XXXXXX XXX X",
-  "X XXX XXo           oXX XXX X",
-  "      XX             XX      ",
+  "X XXX XXXXXXXXXXXXXXXXX XXX X",
+  "X     Xo        XXX  oX     X",
+  "X XXX X               X XXX X",
+  "X XXX X  XXXXBB  X    X XXX X",
+  "      X   B      X    X      ",
+  "X XXX X  XXXX    X    X XXX X",
+  "X XXX X          X   XX XXX X",
+  "X XXX XX       X      X XXX X",
+  "X     X  X            X     X",
+  "X XXX X       X   B   X XXX X",
+  "X XXX X               X XXX X",
+  "X XXX Xo  X        X  oX XXX X",
+  "X XXX XXXXXXXX XXXX XXX XXX X",
+  "X              XXXX         X",
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+};
+
+vector<string> o={
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "X           XX XX           X",
+  "X XXX XXXXX XX XX XXXXX XXX X",
+  "X XXX XXXXX XX XX XXXXX XXX X",
+  "X                           X",
+  "X XXX XXXXXXXXXXXXXXXXX XXX X",
+  "X     XXXoX       XoXXX     X",
   "X XXX XX             XX XXX X",
-  "X XXX XXo           oXX XXX X",
-  "X XXX XXXXXX XXX XXXXXX XXX X",
-  "X     XX     XXX     XX     X",
+  "X XXX XX         B   XX XXX X",
+  "      XX    XXXXX    XX     X ",
+  "X XXX XX   BXXXXX    XX XXX X",
+  "X XXX XX         B   XX XXX X",
+  "X XXX XX   B         XX XXX X",
+  "X     XX   B         XX     X",
   "X XXX XX XXXXXXXXXXX XX XXX X",
   "X XXX                   XXX X",
   "X XXX XXXX XXXXXXXX XXX XXX X",
   "X XXX XXXX          XXX XXX X",
   "X          XXX XXXX         X",
-  "XXXXXXXXXXXXXX XXXXXXXXXXXXXX",
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
+
 
 int main(int argc, char **argv){
     allegro_init();
@@ -57,8 +81,10 @@ int main(int argc, char **argv){
     //Mapa *B=new Mapa1(n);
     Mapa1 A(m);
     Mapa1 B(n);
+    Mapa1 C(o);
     xs.push(A);
     xs.push(B);
+    xs.push(C);
     //xs.top().imprimir();
     Iterador<Mapa1> *it=xs.crearIterador();
     it->inicio();
